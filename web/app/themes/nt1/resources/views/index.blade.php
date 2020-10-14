383451be95a1de197e31481f7adff984565e129c
RD <romualdas@prodev.lt>
2020-10-14 12:50:53 +0300

    Blog posts

--- web/app/themes/nt1/resources/views/index.blade.php
+++ web/app/themes/nt1/resources/views/index.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
-  @if (! have_posts())
-    <x-alert type="warning">
-      {!! __('Sorry, no results were found.', 'sage') !!}
-    </x-alert>
-
-    {!! get_search_form(false) !!}
-  @endif
-
-  @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
-@endsection
-
-@section('sidebar')
-  @include('partials.sidebar')
+  <div class="container py-60px">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-30px">
+      @while(have_posts()) @php(the_post())
+      <a href="{{ get_permalink() }}" class="blog-post relative rounded-4px overflow-hidden">
+        @if($kam = get_field('kam', get_the_ID()))
+          <div class="inline-block absolute top-0 right-0 z-10 bg-blue-1 opacity-85 text-white uppercase text-14px px-6 py-2 mt-4">
+            {{ $kam['label'] }}
+          </div>
+        @endif
+        <figure class="relative block overflow-hidden bg-gray-1 rounded-4px bg-cover bg-no-repeat h-200px transition-opacity duration-500 hover:opacity-75" style="background-image:url({{ get_the_post_thumbnail_url(get_the_ID(), 'nt-objektai') }})"></figure>
+        <h2 class="font-sans font-semibold py-4">{{ get_the_title() }}</h2>
+      </a>
+      @endwhile
+    </div>
+    <div class="my-8">
+      {!! get_the_posts_navigation([
+        'prev_text' => __('Senesni įrašai', 'wm'),
+        'next_text' => __('Naujesni įrašai', 'wm'),
+        'screen_reader_text' => __('Įrašų navigacija', 'wm')
+      ]) !!}
+    </div>
+  </div>
 @endsection
