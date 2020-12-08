abeb471022a5e157e67854ea352fe5b63f43a8ad
RD <romualdas@prodev.lt>
2020-12-08 23:25:52 +0200

    Added default WP content to Apie template

--- web/app/themes/nt1/resources/views/template-about.blade.php
+++ web/app/themes/nt1/resources/views/template-about.blade.php
@@ -11,28 +11,33 @@ $title = get_field('title');
 @extends('layouts.app')
 
 @section('content')
-  <div class="container py-60px">
-    @if(!empty($title))
-      <div class="relative mb-8">
-        <div class="about-title uppercase text-20px md:text-24px font-light">
-          {!! $title !!}
+    <div class="container py-60px">
+        @while(have_posts()) @php(the_post())
+        <div class="prose max-w-none">
+            {!! get_the_content() !!}
         </div>
-        <div class="about-title-separator"></div>
-      </div>
-    @endif
-    <div class="prose max-w-none column-count-2 column-gap-100px mb-50px">
-      {!! get_field('text') !!}
-    </div>
-    <div class="relative text-center">
-      @if(!empty($bottom_grp['line_1']))
-        <div class="text-18px mb-1">{{ $bottom_grp['line_1'] }}</div>
-        <div class="text-12px font-semibold">{{ $bottom_grp['line_2'] }}</div>
-      @endif
-      @if($signature_image_id)
-        <div class="flex justify-center md:absolute top-0 right-0 mt-8 md:mt-0">
-          <img src="{{ wp_get_attachment_image_url($signature_image_id, 'full') }}" alt="">
+        @endwhile
+        @if(!empty($title))
+            <div class="relative mb-8">
+                <div class="about-title uppercase text-20px md:text-24px font-light">
+                    {!! $title !!}
+                </div>
+                <div class="about-title-separator"></div>
+            </div>
+        @endif
+        <div class="prose max-w-none column-count-2 column-gap-100px mb-50px">
+            {!! get_field('text') !!}
         </div>
-      @endif
+        <div class="relative text-center">
+            @if(!empty($bottom_grp['line_1']))
+                <div class="text-18px mb-1">{{ $bottom_grp['line_1'] }}</div>
+                <div class="text-12px font-semibold">{{ $bottom_grp['line_2'] }}</div>
+            @endif
+            @if($signature_image_id)
+                <div class="flex justify-center md:absolute top-0 right-0 mt-8 md:mt-0">
+                    <img src="{{ wp_get_attachment_image_url($signature_image_id, 'full') }}" alt="">
+                </div>
+            @endif
+        </div>
     </div>
-  </div>
 @endsection
