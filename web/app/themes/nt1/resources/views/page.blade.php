f862ebef0f5c43d68a2bb78875850e5d14b719f0
RD <romualdas@prodev.lt>
2020-12-08 23:21:14 +0200

    Default pages template fix

--- web/app/themes/nt1/resources/views/page.blade.php
+++ web/app/themes/nt1/resources/views/page.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @includeFirst(['partials.content-page', 'partials.content'])
+    <div class="container py-60px">
+        <div class="prose max-w-none">
+            @include('partials.page-header')
+            @includeFirst(['partials.content-page', 'partials.content'])
+        </div>
+    </div>
   @endwhile
 @endsection
